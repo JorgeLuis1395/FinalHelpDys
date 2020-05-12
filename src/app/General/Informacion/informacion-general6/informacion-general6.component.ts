03045d29523425d0595f98800a1446d06e8d0461
nchicaiza <38292837+nchicaiza@users.noreply.github.com>
2020-05-11 23:14:21 -0500

    cambios sin regristo

--- src/app/General/Informacion/informacion-general6/informacion-general6.component.ts
+++ src/app/General/Informacion/informacion-general6/informacion-general6.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./informacion-general6.component.css']
 })
 export class InformacionGeneral6Component implements OnInit {
+  mostrarComponente = false;
+  mostrarComponente1 = false;
+  mostrarComponente2 = false;
+  mostrarComponente3 = false;
+  mostrarComponente4 = false;
+  mostrarComponente5 = false;
+  mostrarComponente6 = false;
 
   constructor() { }
 
